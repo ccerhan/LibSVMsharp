2717889d70224c54a45526fefd47c509c14fe476
Can Erhan <can.erhan@bordatech.com>
2018-11-19 13:10:44 +0300

    libsvm version updated to 3.23 (x86 removed, only x64 preserved)

--- LibSVMsharp/Core/libsvm.cs
+++ LibSVMsharp/Core/libsvm.cs
@@ -17,166 +17,90 @@ namespace LibSVMsharp.Core
         /// <param name="prob">svm_problem</param>
         /// <param name="param">svm_parameter</param>
         /// <returns>svm_model</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr svm_train(IntPtr prob, IntPtr param);
         /// <param name="prob">svm_problem</param>
         /// <param name="param">svm_parameter</param>
         /// <param name="nr_fold">int</param>
         /// <param name="target">double[]</param>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern void svm_cross_validation(IntPtr prob, IntPtr param, int nr_fold, IntPtr target);
 
         /// <param name="model_file_name">string</param>
         /// <param name="model">svm_model</param>
         /// <returns>bool</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern int svm_save_model(IntPtr model_file_name, IntPtr model);
         /// <param name="model_file_name">string</param>
         /// <returns>svm_model</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr svm_load_model(IntPtr model_file_name);
 
         /// <param name="model">svm_model</param>
         /// <returns>int</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern int svm_get_svm_type(IntPtr model);
         /// <param name="model">svm_model</param>
         /// <returns>int</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern int svm_get_nr_class(IntPtr model);
         /// <param name="model">svm_model</param>
         /// <param name="label">int[]</param>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern void svm_get_labels(IntPtr model, IntPtr label);
         /// <param name="model">svm_model</param>
         /// <param name="label">int[]</param>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern void svm_get_sv_indices(IntPtr model, IntPtr sv_indices);
         /// <param name="model">svm_model</param>
         /// <returns>int</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern int svm_get_nr_sv(IntPtr model);
         /// <param name="model">svm_model</param>
         /// <returns>double</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern double svm_get_svr_probability(IntPtr model);
 
         /// <param name="model">svm_model</param>
         /// <param name="x">svm_node[]</param>
         /// <param name="dec_values">double[]</param>
         /// <returns>double</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern double svm_predict_values(IntPtr model, IntPtr x, IntPtr dec_values);
         /// <param name="model">svm_model</param>
         /// <param name="dec_values">double[]</param>
         /// <returns>double</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern double svm_predict(IntPtr model, IntPtr x);
         /// <param name="model">svm_model</param>
         /// <param name="x">svm_node[]</param>
         /// <param name="dec_values">double[]</param>
         /// <returns>double</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern double svm_predict_probability(IntPtr model, IntPtr x, IntPtr prob_estimates);
 
         /// <param name="model_ptr">svm_model</param>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern void svm_free_model_content(IntPtr model_ptr);
         /// <param name="model_ptr_ptr">svm_model*</param>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern void svm_free_and_destroy_model(ref IntPtr model_ptr_ptr);
         /// <param name="param">svm_parameter</param>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern void svm_destroy_param(IntPtr param);
 
         /// <param name="prob">svm_problem</param>
         /// <param name="param">svm_parameter</param>
         /// <returns>string</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr svm_check_parameter(IntPtr prob, IntPtr param);
         /// <param name="model">svm_model</param>
         /// <returns>int</returns>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern bool svm_check_probability_model(IntPtr model);
 
         /// <param name="print_function">void (*)(const char *)</param>
-#if WIN32
-        [DllImport("libsvm.dll", CallingConvention = CallingConvention.Cdecl)]
-#else
-        [DllImport("libsvm_x64.dll", CallingConvention = CallingConvention.Cdecl)]
-#endif
+        [DllImport("libsvm", CallingConvention = CallingConvention.Cdecl)]
         public static extern void svm_set_print_string_function(IntPtr print_function);
     }
 }
