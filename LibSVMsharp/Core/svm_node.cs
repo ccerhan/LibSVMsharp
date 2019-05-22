e7720fedcea487729468b12969f078c6c48d1c41
LWChris <lwchris@gmx.de>
2019-05-22 17:15:59 +0200

    All SV in saved model have 0 nodes

--- LibSVMsharp/Core/svm_node.cs
+++ LibSVMsharp/Core/svm_node.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.InteropServices;
 
 namespace LibSVMsharp.Core
 {
     [StructLayout(LayoutKind.Sequential)]
     public struct svm_node
     {
-        internal int index;
-        internal double value;
+        public int index;
+        public double value;
     }
 }
